f4f1e367f05d0c29b8239ef545cf6a4367b58d01
Robson Lunardelli <robson@iscfed.com>
2014-12-10 14:13:08 -0700

    github payload

--- test/github.php
+++ test/github.php
@@ -1,1 +1,16 @@
-<?php `git pull`;
+<?php 
+	try
+	{
+		$payload = json_decode($_REQUEST['payload']);
+	}
+	catch(Exception $e)
+	{
+		exit(0);
+	}
+
+	file_input_contents('github.log', print_r($payload, TRUE), FILE_APPEND);
+
+	if ($payload->ref == 'refs/heads/master')
+	{
+		`git pull`;
+	}
